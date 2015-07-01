92da245c417db34a58f985860637f795096e263c
Ken <khwken@gmail.com>
2015-06-30 17:28:37 -0700

    Setup express server and rethinkDB

---
+++ server/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var session = require('express-session');
+var r = require('rethinkdb');
+
+
+var app = express();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('ReactAuth app running on' + port);
+
+});
